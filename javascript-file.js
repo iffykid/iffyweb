c4b58a0861c9ea237921cf6ae006a545644ea71a
iffykid <shaunmusodza@gmail.com>
2022-12-14 13:19:24 +0200

    new product display

--- javascript-file.js
+++ javascript-file.js
@@ -26,28 +26,30 @@ function showSlides(n) {
   setTimeout(showSlides, 2000);
 }
 
-document.getElementById("my-button").addEventListener("click", function() {
-  // Do something when the button is clicked
-  alert("Link was clicked");
-});
+function openCloseModal() {
+  // Get the modal
+  var modal = document.getElementById("myModal");
 
-// Get the modal
-var modal = document.getElementById("myModal");
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  // When the user clicks the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 }
