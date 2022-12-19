92cb6c66ceb6a55612f9a0c4c0d719648127f66c
iffykid <shaunmusodza@gmail.com>
2022-12-20 01:05:12 +0200

    working product

--- javascript-file.js
+++ javascript-file.js
@@ -23,23 +23,34 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000);
+  setTimeout(showSlides, 5000);
 }
 
-function openCloseModal() {
+
+function openCloseModal(input) {
   // Get the modal
   var modal = document.getElementById("myModal");
 
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
-
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks the button, open the modal
-  btn.onclick = function() {
     modal.style.display = "block";
-  }
+      const paragraph = document.getElementById("modalText");
+      if(input === "logo-design"){
+      paragraph.textContent = "Logo design is a crucial element of any business's branding and marketing strategy. It's the visual representation of your company's identity, and it helps customers to quickly and easily recognize and remember your brand. A well-designed logo can help to differentiate your business from your competitors and create a strong, professional image that inspires trust and confidence in your customers. Our logo design services provide your business with a unique, eye-catching design that accurately reflects your brand's values and personality, helping you to stand out in a crowded market and attract more customers. Whether you're starting a new business or looking to revamp your existing branding, our team of experienced designers can help you create a logo that effectively communicates your message and resonates with your target audience.";
+      }
+      else if(input === "web-design"){
+        paragraph.textContent = "Web design is a crucial element of any business's online presence and marketing strategy. It's the visual representation of your company's online identity, and it helps customers to quickly and easily navigate and interact with your website. A well-designed website can help to differentiate your business from your competitors and create a strong, professional image that inspires trust and confidence in your customers. Our web design services provide your business with a unique, user-friendly design that accurately reflects your brand's values and personality, helping you to stand out in a crowded market and attract more customers. Whether you're starting a new business or looking to revamp your existing website, our team of experienced designers can help you create a website that effectively communicates your message and resonates with your target audience.";
+      }
+      else if(input === "social-media"){
+        paragraph.textContent = "Our social media design services provide your business with professional, visually appealing designs for your social media platforms. From profile pictures and cover photos to posts and ads, we can help you create a cohesive and on-brand social media presence that effectively communicates your message and resonates with your target audience. Let us help you make a strong impression on social media and stand out in a crowded market.";
+      }
+      else if(input === "aa-edit"){
+        paragraph.textContent = "Our album art design and graphic edit services provide your business or personal brand with visually stunning and professional designs for your music releases and promotional materials. From custom album covers and liner notes to promotional posters and social media graphics, we can help you create a cohesive and on-brand visual identity that effectively communicates your message and resonates with your target audience. We can also provide customised wallpapers and pictures for your personal or professional use, helping you to add a unique and professional touch to your digital spaces. Let us help you make a strong visual impact and stand out in your industry.";
+      }
+      else if(input === "customized-apparel"){
+        paragraph.textContent = "Our customised apparel services provide you or your business or personal brand with high-quality and professionally designed clothing options. From graphic tees to hats and hoodies, we can help you create a cohesive and on-brand wardrobe that effectively communicates your message and resonates with your target audience. We offer a wide range of popular graphic tees and can work with you to design custom apparel that is unique to your brand. Our team of experienced designers and manufacturers ensures that every piece of clothing is of the highest quality and meets your exact specifications. Let us help you make a strong fashion statement and stand out in your industry.";
+      }
+
 
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
